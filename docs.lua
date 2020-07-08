2af9a0473f0e75ce0895c38f39fe5a2a213ec5e9
BytecodexDev <54802419+BytecodexDev@users.noreply.github.com>
2020-07-08 08:56:24 +0800

    Create docs.lua

---
+++ docs.lua
@@ -0,0 +1,5 @@
+-- [[ ByteSecure X Documentation
+    BSX_create_secure_code(code <string>, watermarkEnabled <bool>, copyToClipboard <bool>)
+    - Encrypts the code securely and saves it to the workspace folder or at the clipboard
+    BSX_run_secure_code(code <string>, key <string>, iv <string>)
+]]
